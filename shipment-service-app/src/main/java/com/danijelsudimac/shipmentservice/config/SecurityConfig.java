1f022f8da23983b9c6fe56cf90f7b629941acd76
Danijel Sudimac <dacha.sudimac@gmail.com>
2026-05-26 01:04:08 +0200

    feat: Add task definition for fargate

--- shipment-service-app/src/main/java/com/danijelsudimac/shipmentservice/config/SecurityConfig.java
+++ shipment-service-app/src/main/java/com/danijelsudimac/shipmentservice/config/SecurityConfig.java
@@ -3,30 +3,41 @@ package com.danijelsudimac.shipmentservice.config;
 import com.danijelsudimac.shipmentservice.security.ApiKeyAuthFilter;
 import com.danijelsudimac.shipmentservice.service.ApiKeyConfigurationService;
 import com.danijelsudimac.shipmentservice.service.RateLimitService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final RateLimitService rateLimitService;
     private final ApiKeyConfigurationService apiKeyConfigurationService;
 
-    public SecurityConfig(RateLimitService rateLimitService, ApiKeyConfigurationService apiKeyConfigurationService) {
-        this.rateLimitService = rateLimitService;
-        this.apiKeyConfigurationService = apiKeyConfigurationService;
+    @Bean
+    @Order(1)
+    SecurityFilterChain actuatorChain(HttpSecurity http)
+            throws Exception {
+
+        http.securityMatcher(
+                        "/actuator/**",
+                        "/api/apikey-policy")
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/health").permitAll()
-                        .requestMatchers("/api/apikey-policy").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new ApiKeyAuthFilter(rateLimitService, apiKeyConfigurationService),
