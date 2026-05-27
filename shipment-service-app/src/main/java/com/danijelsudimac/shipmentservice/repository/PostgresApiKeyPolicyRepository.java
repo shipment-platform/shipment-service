508eaceaa6c220596f963a26b65b9df77f4a53fb
Danijel Sudimac <dacha.sudimac@gmail.com>
2026-05-27 21:35:02 +0200

    fix: Integration test and database url secret mapping in yaml

--- shipment-service-app/src/main/java/com/danijelsudimac/shipmentservice/repository/PostgresApiKeyPolicyRepository.java
+++ shipment-service-app/src/main/java/com/danijelsudimac/shipmentservice/repository/PostgresApiKeyPolicyRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 
-public interface PostgresApiKeyPolicyRepository extends JpaRepository<ApiKeyPolicy, Long> {
+public interface PostgresApiKeyPolicyRepository extends JpaRepository<ApiKeyPolicy, Long>, ApiKeyPolicyRepository {
     Optional<ApiKeyPolicy> findByClientId(Long clientId);
 
     Optional<ApiKeyPolicy> findByApiKey(String apiKey);
