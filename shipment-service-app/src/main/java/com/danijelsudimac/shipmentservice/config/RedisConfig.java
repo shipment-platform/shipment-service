3324c65a319b00ecc89dce94c17d4d0b640c9ade
Danijel Sudimac <dacha.sudimac@gmail.com>
2026-05-31 01:23:43 +0200

    fix: Fix lettuce config not loaded

---
+++ shipment-service-app/src/main/java/com/danijelsudimac/shipmentservice/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.danijelsudimac.shipmentservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@Profile("prod")
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${SPRING_DATA_REDIS_HOST}") String host,
+            @Value("${SPRING_DATA_REDIS_PORT}") int port,
+            @Value("${SPRING_DATA_REDIS_USERNAME}") String username,
+            @Value("${SPRING_DATA_REDIS_PASSWORD}") String password) {
+
+        RedisStandaloneConfiguration redisConfig =
+                new RedisStandaloneConfiguration(host, port);
+
+        redisConfig.setUsername(username);
+        redisConfig.setPassword(password);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .useSsl()
+                        .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
+}
